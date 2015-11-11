a24943f4a3fbcc84810bc38bb1b663c4edd8f0e2
karthik <karthigayan.n@gmail.com>
2015-11-12 00:07:16 +0400

    change back to world

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello emirates');
+  res.send('hello world');
 });
  
 app.listen(process.env.PORT || 5000);
