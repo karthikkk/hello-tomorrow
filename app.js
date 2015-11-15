7da5f4c5ebeb0faafb49ca9f073b460319a1e922
karthik <karthigayan.n@gmail.com>
2015-11-15 12:30:43 +0400

    change it emirates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello Emirates˚
+  res.send('hello Emirates˚');
 });
  
 app.listen(process.env.PORT || 5000);
